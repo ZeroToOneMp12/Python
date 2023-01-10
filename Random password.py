314ada87aacfa9e9aa0e2a6eff471b97020a5f32
Your Name <mathieu.pla.07@gmail.com>
2023-01-10 08:21:18 +0100

    version OK

--- Random password.py
+++ Random password.py
@@ -6,18 +6,19 @@ import string
 mot_de_passe = input("Entrez le mot de passe a trouver: ")
 
 # Fonction qui genere un mot de passe aleatoire
-def mot_aleatoire(longueur):
+def mot_aleatoire():
     lettres = string.ascii_letters
     suiv = ""
     resultat = ""
     for i in range (len(mot_de_passe)):
         while mot_de_passe[i] != suiv: 
+            print(resultat + suiv)
             suiv = random.choice(lettres)
-    resultat += suiv
+        resultat += suiv
     return resultat
 
 deb = time.time()
-mot_aleatoire()
+print("mot_aleatoire " + mot_aleatoire())
 print("Temps d'execution: " + str(time.time() - deb) + " secondes")
 
 # Boucle qui genere des mots de passes aleatoires jusqu'a ce qu'il trouve le bon
